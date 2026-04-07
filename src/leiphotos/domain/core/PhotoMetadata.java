d161b90a2ee3b34589526a88a558d6a0b179ade1
TomasPeres <fc63721@alunos.fc.ul.pt>
2026-04-07 15:12:59 +0100

    PhotoMetadata_fix

--- src/leiphotos/domain/core/PhotoMetadata.java
+++ src/leiphotos/domain/core/PhotoMetadata.java
@@ -2,11 +2,34 @@ package leiphotos.domain.core;
 
 import java.time.LocalDateTime;
 
-public class PhotoMetadata {
+import leiphotos.utils.RegExpMatchable;
+
+public class PhotoMetadata implements RegExpMatchable{
+    // Associação
+    private GPSLocation location;
+
+    private String camera;
+    private String manufacturer;
+    private String aperture;
+    private LocalDateTime date;
+
 
     PhotoMetadata(String camera, String manufacturer, LocalDateTime date, String aperture,
             double[] gpsLocation) {
+                this.camera = camera;
+                this.manufacturer = manufacturer;
+                this.aperture = aperture;
+                this.date = date;
 
+                if(gpsLocation != null && gpsLocation.length <= 2){
+                    this.location = new GPSLocation();
+                }
+    }
+
+    @Override
+    public boolean matches(String regexp) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'matches'");
     }
 
 }
